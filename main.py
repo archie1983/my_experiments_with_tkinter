949e6377ba8c7916b6470c04abf438d9220d6721
Arturs Elksnis <archie1983@hotmail.co.uk>
2020-06-05 00:10:59 +0100

    Adding buttons.

--- main.py
+++ main.py
@@ -6,22 +6,34 @@ Created on Thu Jun  4 13:30:44 2020
 """
 import tkinter as tk
 
-def createMainClassesWindow():
-    #space_between_lbl_and_txt = 
-    window = tk.Tk()
-    
+window = tk.Tk()
+txt_name_val = tk.StringVar()
+txt_descr_val = tk.StringVar()
+
+def createMainClassesWindow():    
     # Name of the item class
     lblName = tk.Label(window, text="Class name", fg="black", font=("Arial", 10), width="10")
     lblName.grid(row=0, column=0)
-    txtName = tk.Text(window, height=1, width=30)
+    txtName = tk.Entry(window, width=30, textvariable=txt_name_val)
     #txtName.insert(tk.END, "Class name")
     txtName.grid(row=0, column=1)
 
     # Description of the item class
     lblDescr = tk.Label(window, text="Class description", fg="black", font=("Arial", 10))
     lblDescr.grid(row=1, column=0)
-    txtDescr = tk.Text(window, height=1, width=30)
+    txtDescr = tk.Entry(window, width=30, textvariable=txt_descr_val)
     txtDescr.grid(row=1, column=1)
+
+    btnAddClass = tk.Button(window,
+                       text="Add Class",
+                       command=add_class)
+    btnAddClass.grid(row=2, column=0)
+    
+    button = tk.Button(window, 
+                       text="QUIT", 
+                       fg="red",
+                       command=window.destroy)
+    button.grid(row=2, column=1)
     
     # add title
     window.title("Check Out Data Entry - Main Classes")
@@ -31,6 +43,9 @@ def createMainClassesWindow():
     
     window.mainloop()
     
-    
+def add_class():
+    txt_name_val.set("AAAAA")
+    txt_descr_val.set("BBBB")
+
 if __name__ == '__main__':
     createMainClassesWindow()
