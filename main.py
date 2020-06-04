b39840bc6f43a5856b9c10c81cea412b0597d861
Arturs Elksnis <archie1983@hotmail.co.uk>
2020-06-04 20:12:42 +0100

    Second attempt with grid layout management.

--- main.py
+++ main.py
@@ -7,26 +7,21 @@ Created on Thu Jun  4 13:30:44 2020
 import tkinter as tk
 
 def createMainClassesWindow():
-    row_top_spacing = 27
-    row_top = 10
-    row_left = 10
     #space_between_lbl_and_txt = 
     window = tk.Tk()
     
     # Name of the item class
     lblName = tk.Label(window, text="Class name", fg="black", font=("Arial", 10), width="10")
-    lblName.place(x=row_left, y=row_top)
+    lblName.grid(row=0, column=0)
     txtName = tk.Text(window, height=1, width=30)
     #txtName.insert(tk.END, "Class name")
-    txtName.place(x=100, y=row_top)
-    row_top += row_top_spacing
+    txtName.grid(row=0, column=1)
 
     # Description of the item class
     lblDescr = tk.Label(window, text="Class description", fg="black", font=("Arial", 10))
-    lblDescr.place(x=row_left, y=row_top)
+    lblDescr.grid(row=1, column=0)
     txtDescr = tk.Text(window, height=1, width=30)
-    txtDescr.place(x=100, y=row_top)
-    row_top += row_top_spacing
+    txtDescr.grid(row=1, column=1)
     
     # add title
     window.title("Check Out Data Entry - Main Classes")
